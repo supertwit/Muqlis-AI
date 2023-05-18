5651b5f2fb3292427a24c2f0100eef1ef14498f5
hamid <hussainhafeez1100@gmail.com>
2023-05-18 11:45:28 +0530

    integrated google sigin

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -25,6 +25,15 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
+
 export default function Navbar() {
   return (
     <Disclosure as="nav" style={{background: "linear-gradient(90deg,var(--brand-2) 0%,var(--brand-1) 98%)"}}>
@@ -76,15 +85,15 @@ export default function Navbar() {
                 </div>
               </div>
               <div className="flex items-center">
-                <div className="flex-shrink-0">
+                {/* <div className="flex-shrink-0">
                   <button
                     type="button"
                     className="relative inline-flex items-center gap-x-1.5 rounded-md bg-red-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
                   >
                     <FaceSmileIcon className="mr-1 h-5 w-5" aria-hidden="true" />
-                    Signin with Google  
                   </button>
-                </div>
+                </div> */}
+                    <div class="g-signin2" data-onsuccess={onSignIn}></div>
                 <div className="hidden">
                   <button
                     type="button"
